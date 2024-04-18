53ad8b60d7cb834c55bc13aabf770dc45b865c58
Alex Berman <alexander.berman@gu.se>
2024-04-18 11:39:35 +0200

    Add unit test for ComplexTypesSystem.create_ptype

--- test/test_typesystem.py
+++ test/test_typesystem.py
@@ -1,4 +1,4 @@
-from typesystem import Type, BasicTypesSystem
+from typesystem import Type, BasicTypesSystem, Predicate, PredicateSignature, ComplexTypesSystem
 
 
 class TestBasicTypesSystem:
@@ -10,7 +10,32 @@ class TestBasicTypesSystem:
         def extension_function(x):
             if x == dog:
                 return {sally}
+            return {}
 
         system = BasicTypesSystem({dog}, extension_function)
         assert system.check(sally, dog)
         assert not system.check(sam, dog)
+
+
+class TestComplexTypesSystem:
+    def test_create_ptype(self):
+        """Corresponds to Cooper (2023, p.16)"""
+        ind = Type('ind')
+
+        def extension_function(x):
+            return {}
+
+        basic_types_system = BasicTypesSystem({ind}, extension_function)
+        boy = Predicate('boy')
+        dog = Predicate('dog')
+        hug = Predicate('hug')
+
+        def arity(x):
+            if x in [boy, dog]:
+                return (ind,)
+            elif x == hug:
+                return (ind, ind)
+
+        predicate_signature = PredicateSignature(predicates={boy, dog, hug}, arg_indices={ind}, arity=arity)
+        system = ComplexTypesSystem(basic_types_system, predicate_signature, extension_function)
+        assert system.create_ptype(hug, (boy, dog)) == {'pred': hug, 0: boy, 1: dog}
